2fae03178c33d49ae8bac38af9d65005e4ada230
serzilo <designkota@mail.ru>
2016-09-11 14:46:33 +0300

    search page styles

--- src/js/components/MainPage.jsx
+++ src/js/components/MainPage.jsx
@@ -7,6 +7,9 @@ var React = require('react'),
 
 var MainPage = React.createClass({
 	componentDidMount: function () {
+		this.searchInputFocus();
+	},
+	searchInputFocus: function () {
 		this.refs.searchInput.focus();
 	},
 	handleSubmit: function (event) {
@@ -16,8 +19,9 @@ var MainPage = React.createClass({
 
 		if (query.length > 0) {
 			hashHistory.push({pathname: '/search', query: {s: query}});
+		} else {
+			this.searchInputFocus();
 		}
-
 	},
 	render: function () {
 		return (
@@ -26,11 +30,13 @@ var MainPage = React.createClass({
 					Поиск фильмов
 				</Title>
 
-				<div>
-					<form onSubmit={this.handleSubmit}>
-						<input ref="searchInput" type="text" placeholder="Введите фильм" />
-						<button type="submit">Найти</button>
-					</form>
+				<div className="container container_search">
+					<div className="search">
+						<form onSubmit={this.handleSubmit}>
+							<input className="search__input" ref="searchInput" type="text" placeholder="Введите фильм" />
+							<button className="search__submit" type="submit">Найти</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		);
